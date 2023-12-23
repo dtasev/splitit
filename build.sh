5c5309955d61cccfde27cfb833b8935c7c154b50
dtasev <dimtasev@gmail.com>
2023-12-23 19:28:41 +0000

    Back to uploading dist because the docs lied

--- build.sh
+++ build.sh
@@ -6,4 +6,3 @@ source ./base.sh
 docker build -t ${image_name} -f Dockerfile .
 docker tag ${image_name} $image_repo:latest
 docker run -t -v $(pwd)/app:/app:rw -w /app ${image_name} /bin/bash -c "npm install && npx vite build $1 $2"
-cd app && tar -czvf dist.tar.gz dist/ && cd ..
