3bbca68966825bd98e872fa75c027f20a43182af
dtasev <dimtasev@gmail.com>
2023-12-23 19:24:52 +0000

    update build & tar

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@ source ./base.sh
 docker build -t ${image_name} -f Dockerfile .
 docker tag ${image_name} $image_repo:latest
 docker run -t -v $(pwd)/app:/app:rw -w /app ${image_name} /bin/bash -c "npm install && npx vite build $1 $2"
-cd app && tar -czvf archive_name.tar.gz folder_name && cd ..
+cd app && tar -czvf dist.tar.gz dist/ && cd ..
