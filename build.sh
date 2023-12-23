624f3a003bc215f5de81a1259d55a74a66d4d9ed
dtasev <dimtasev@gmail.com>
2023-12-23 19:32:27 +0000

    Set correct --base=https://dtasev.github.io/splitit/

--- build.sh
+++ build.sh
@@ -6,3 +6,4 @@ source ./base.sh
 docker build -t ${image_name} -f Dockerfile .
 docker tag ${image_name} $image_repo:latest
 docker run -t -v $(pwd)/app:/app:rw -w /app ${image_name} /bin/bash -c "npm install && npx vite build $1 $2"
+git add -f app/dist/
