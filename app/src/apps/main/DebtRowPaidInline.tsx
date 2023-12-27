03b7fd7adde39b812e18a29f37e3906405c4273c
dtasev <dimtasev@gmail.com>
2023-12-27 17:39:44 +0000

    rewrite the debt logic again

--- app/src/apps/main/DebtRowPaidInline.tsx
+++ app/src/apps/main/DebtRowPaidInline.tsx
@@ -6,21 +6,31 @@ interface DebtRowPaidInlineIP {
     debt: DebtApiResponse
 }
 export default memo(function DebtRowPaidInline(props: PropsWithChildren<DebtRowPaidInlineIP>) {
-    let phraseL: string, phraseR: string, owed: number;
+    let phraseL: string, phraseR: string;
     if (props.owner) {
         phraseL = "you paid";
         phraseR = `you lent ${props.debt.is_owed_username}`;
-        owed = props.debt.amount * props.debt.ratio / 100;
     } else {
         phraseL = `${props.debt.owner_username} paid`;
         phraseR = `${props.debt.owner_username} lent you`;
-        owed = props.debt.amount * Math.abs(100 - props.debt.ratio) / 100;
     }
+
+    let ratio: number;
+    switch (props.debt.ratio) {
+        case 100:
+        case 0:
+            ratio = 1
+            break
+        default:
+            ratio = (100 - props.debt.ratio) / 100;
+    }
+
+    const owed = props.debt.amount * ratio;
 
     return (<Row className='col-lg-6 col-12'>
         <Col className='col-6'>
             <div className='text-center font-little'>{phraseL}</div>
-            <div className='text-center fw-bold'>£{props.debt.amount}</div>
+            <div className='text-center fw-bold'>£{props.debt.amount} ({props.debt.ratio}%)</div>
         </Col>
         <Col className='col-6'>
             <div className='text-center font-little'>{phraseR}</div>
--- app/src/apps/main/SideBarRight.tsx
+++ app/src/apps/main/SideBarRight.tsx
@@ -13,22 +13,30 @@ export default memo(function SideBarRight(props: PropsWithChildren<SideBarRightI
         // if the user is not the owner then they owe the ratio of the debt
         // e.g. if user1 paid £50 for user2 with a ratio of 100 (i.e. user1 paid 100% of the £50) -> user2 owes the full amount
         // if user1 paid £50 for user2 with a ratio of 75 (i.e. user1 paid 75% of the £50) -> user2 owes the remaining 25% of 50
-        if (cv.owner_username === userCtx.username) {
-            return acc + (cv.amount * cv.ratio / 100);
-        } else {
-            return acc - (cv.amount * Math.abs(100 - cv.ratio) / 100);
-        }
-        //   || cv.is_owed_username !== userCtx.username)
 
+        let ratio: number;
+        switch (cv.ratio) {
+            case 100:
+            case 0:
+                ratio = 1
+                break
+            default:
+                ratio = (100 - cv.ratio) / 100;
+        }
+        if (cv.owner_username === userCtx.username) {
+            return acc + (cv.amount * ratio);
+        } else {
+            return acc - (cv.amount * ratio);
+        }
     }, 0);
 
     let msg = "";
     if (totalOwed === 0) {
         msg = "owe nothing to no one."
     } else if (totalOwed > 0) {
-        msg = `owe £${totalOwed}`
+        msg = `are owed £${totalOwed}`
     } else {
-        msg = `are owed £${Math.abs(totalOwed)}`
+        msg = `owe £${Math.abs(totalOwed)}`
     }
     return (<Container>You {msg}</Container>);
 })
