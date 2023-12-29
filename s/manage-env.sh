a37b0edf8b6c1ea377462b064063ce6b8bb4ce18
dtasev <dimtasev@gmail.com>
2023-12-29 23:43:55 +0000

    encrypt in base64

--- s/manage-env.sh
+++ s/manage-env.sh
@@ -5,7 +5,7 @@ set -ex
 if [ "$1" = "encrypt" ]
 then
     echo "Encrypting .env to .env.encrypted"
-    openssl enc -aes-256-cbc -salt -in .env -out .env.encrypted
+    openssl enc -aes-256-cbc -a -salt -in .env -out .env.encrypted
 else
-    openssl enc -d -aes-256-cbc -in .env.encrypted -out .env -pass pass:$1
+    openssl enc -d -aes-256-cbc -a -in .env.encrypted -out .env -pass pass:$1
 fi
